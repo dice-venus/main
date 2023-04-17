6cbf046428ba43053d9f7c7268c896097a816445
dice <diceofvenus.cs@gmail.com>
2023-04-16 21:21:53 -0500

    Fixed VSC

---
+++ C/test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    int x = 3;
+    printf("%i", x);
+
+    return 0;
+}
---
+++ C/test2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    int y = 2;
+    printf("%i", y);
+
+    return 0;
+}
